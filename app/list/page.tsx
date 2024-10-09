5a55bb79cc33668c6e880e617e7aca3453a379dd
Guilherme MEira <contact@techmakers.online>
2024-10-09 09:39:20 +0200

    new list

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
     
-    <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-b from-transparent to-white z-30 before:absolute before:inset-0 before:bg-white-100 before:bg-opacity-10 before:bottom-0" style={{ height: '500px' }}></div>
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-b from-transparent to-[#faf9f6] z-20 before:absolute before:inset-0 before:bg-[#faf9f6] before:bg-opacity-10 before:bottom-0" style={{ height: '500px' }}></div>
         <AnimatedListDemo />
     </>
   );
---
+++ app/list_new/page.tsx
@@ -0,0 +1,11 @@
+import {AnimatedListDemo2} from '@/components/AnimatedList2'
+
+export default function Home() {
+  return (
+    <>
+    
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-b from-transparent to-[#faf9f6] z-50 before:absolute before:inset-0 before:bg-[#faf9f6] before:bg-opacity-10 before:bottom-0" style={{ height: '500px' }}></div>
+        <AnimatedListDemo2 />
+    </>
+  );
+}
